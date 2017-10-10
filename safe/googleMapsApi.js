d84027a1ef40f677ca0b890c445b62f8a2855a02
KiwonNam2016 <kiwonnam2016@u.northwestern.edu>
2017-10-09 20:59:35 -0500

    Revert "Merge branch 'google-maps-api' into development"
    This reverts commit 5ee15fb2b9664e29d2d26fb9a45520185924349b, reversing
    changes made to a17a310c6da30f13e5ecc18d3dfd461cdba6c5be.

--- safe/googleMapsApi.js
+++
@@ -1,9 +0,0 @@
-//Google Maps API
-
-var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-  		center: {lat: 41.881832, lng: -87.623177},
-        zoom: 17
-	});
-}
