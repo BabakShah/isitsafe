291c7eb0486fec3e1c056d7e3775e3569aad2b30
KiwonNam2016 <kiwonnam2016@u.northwestern.edu>
2017-10-11 23:41:18 -0500

    "changed body color to black to see letters, very basic on-click function for submit button"

--- safe/assets/js/google-maps-api.js
+++ safe/assets/js/google-maps-api.js
@@ -1,15 +1,46 @@
 //$(document).ready(function() {
-    var map;
-	    function initMap() {
-	    	var location = {lat: 41.881832, lng: -87.623177};
-	    	map = new google.maps.Map(document.getElementById('map'), {
-	        center: location,
-	        zoom: 11
-	    });
-	    var marker = new google.maps.Marker({
-	    	position: location,
-	    	map: map
-	    });
+    
+
+    //location = $("#location").val();
+
+
+	var map;
+    var loc;
+
+
+	$('#submit-button').on("click",function(event) {
+		event.preventDefault();
+		console.log($('#location').val());
+		loc = $("#location").val();
+		var locationArray = loc.split(',');
+		lat = parseInt(locationArray[0]);
+		lng = parseInt(locationArray[1]);
+		console.log(locationArray);
+		console.log(loc);
+	});
+
+
+    function initMap() {
+    	var location = {lat: 41.881832, lng: -87.623177};
+    	map = new google.maps.Map(document.getElementById('map'), {
+        center: location,
+        zoom: 11
+    });
+    var marker = new google.maps.Marker({
+    	position: location,
+    	map: map
+    });
+	/*
+	var marker = new google.maps.Marker({
+		position: location,
+		map: map
+	});*/
+
+
 	}
+
+	
+
+
 //});
 	
