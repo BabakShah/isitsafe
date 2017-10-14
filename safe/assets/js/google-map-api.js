e54e0aad45a7cd3253459f4e4f8fa81d23e1c9ad
KiwonNam2016 <kiwonnam2016@u.northwestern.edu>
2017-10-14 17:44:50 -0500

    "split into js files writing to table and autocomplete are not working for some reason"

---
+++ safe/assets/js/google-map-api.js
@@ -0,0 +1,22 @@
+var map;
+
+//renders the google map
+function initMap() {
+    map = new google.maps.Map(
+        document.getElementById('map'), 
+        {
+            center: {lat: 41.881832, lng: -87.623177},
+            zoom: 11
+        }
+    );
+}
+
+//function that adds marker to the map where the crime was commited, takes longitude, latitude
+function addMarker(lat,lng){
+    var marker;
+    marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat,lng),
+            map: map
+        }
+    );
+}
